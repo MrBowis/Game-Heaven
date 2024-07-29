6d0b425b9229a1b1264c7eea7a481d7d6c61d0b8
Allan Panchi <avpanchi@espe.edu.ec>
2024-07-28 22:05:45 -0500

    Creacion de Rutas

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
-//import HomePage from "./Pages/HomePage";
-//import Register from './Pages/Register';
+import HomePage from "./Pages/HomePage";
+import Register from './Pages/Register';
 import Login from './Pages/Login';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
 	return (
 		<>
-			<Login></Login>
+			<Router>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/login" element={<Login />} />
+				</Routes>
+			</Router>
 		</>
 	);
 }
